83b1d4e91f11f4b4fe5afbdb79f2aac83522f6b0
ColtonCast <coltonjett400@gmail.com>
2018-10-11 14:57:42 -0700

    added globe to search page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -429,45 +429,9 @@ $(document).ready(function () {
             })
           })
   }
+});
 
-// //=========p5js background============
-// var img;
-// var angle = 0;
-// var canvas;
-// // function preload() {
-// //  // preload() runs once
 
-// // }
-
-//=========p5js background============
-var img;
-var angle = 0;
-var xangle = 0;
-var canvas;
-function preload() {
-  img = loadImage("assets/images/1_earth_8k.jpg");
-}
-
-function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
-}
-
-function draw() {
-  camera(0, 200, (height / 2) / tan(PI / 6), -200, 0, 0, 0, 1, 0);
-  background(0, 100, 200);
-  rectMode(CENTER);
-  rotateY(angle);
-  rotateX(xangle);
-  texture(img);
-  sphere(width / 3, 24, 24);
-  xangle += .0002;
-  angle += .001;
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
-//=========== end p5js background==========
 
 //==============firebase============
 
@@ -483,4 +447,4 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 //=============firebase================
-});
+
